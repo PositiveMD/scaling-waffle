1b1ea1e5a48d4319ede7ee9c01e60e316d4c6ea5
Anthony Dinh <adinh182@knights.ucf.edu>
2016-02-11 08:11:55 -0500

    create enum files for ExchangerPackager

---
+++ ED-Tree/src/State.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Anthony Dinh on 2/11/2016.
+ */
+public enum State {
+    ELIMINATED, TOGGLE, DIFFRACTED0, DIFFRACTED1
+}
---
+++ ED-Tree/src/Type.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Anthony Dinh on 2/11/2016.
+ */
+public enum Type {
+    
+}
