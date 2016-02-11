71f37c58b357b5185c75d0cc91d2559ee9e23a73
Anthony Dinh <adinh182@knights.ucf.edu>
2016-02-11 10:48:44 -0500

    add constructor for ED Tree

--- ED-Tree/src/Balancer.java
+++ ED-Tree/src/Balancer.java
@@ -7,4 +7,11 @@ public class Balancer {
     Balancer leftChild, rightChild;
     ThreadLocal<Integer> lastSlotRange;
 
+    /**
+     *  Creates a tree of N/2 levels of balancers that contain N queues where N is the number inputted.
+     */
+    public Balancer(int numQueues)
+    {
+
+    }
 }
