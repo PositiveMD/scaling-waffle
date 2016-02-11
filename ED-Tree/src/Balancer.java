b69815082bd4e41d007a6f7d1f02b8f71569bae7
Anthony Dinh <adinh182@knights.ucf.edu>
2016-02-11 11:03:22 -0500

    balancer now has a constructor

--- ED-Tree/src/Balancer.java
+++ ED-Tree/src/Balancer.java
@@ -2,10 +2,18 @@
  * Created by Anthony on 2/2/2016.
  */
 public class Balancer {
+    public static final int ELIMINATIONARRAYSIZE = 10;
+
     ToggleBit producerToggle, consumerToggle;
     Exchanger[] eliminationArray;
     Balancer leftChild, rightChild;
     ThreadLocal<Integer> lastSlotRange;
 
-
+    //TODO : Figure out what order to do the linking for the left and right child
+    public Balancer(){
+        producerToggle = new ToggleBit();
+        consumerToggle = new ToggleBit();
+        eliminationArray = new Exchanger[ELIMINATIONARRAYSIZE];
+        lastSlotRange = new ThreadLocal<>();
+    }
 }
