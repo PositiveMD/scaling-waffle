a532a4175b59290a548402b9f8f992ecb95f8126
mahrab <barham@knights.ucf.edu>
2016-02-12 15:38:39 -0500

    Test.java
    A class that will implement the test, and contains the main method. It will construct an ED-Tree object, spawn threads to make requests of it, and evaluate performance. (It's pretty empty right now, though)

---
+++ ED-Tree/src/Test.java
@@ -0,0 +1,11 @@
+// Created by Clayton Barham on 2/12/2016
+
+public class Test
+{
+  public static void main(String[] args)
+  {
+    // Instantiate ED-Tree
+    // Spawn threads to make push and pop requests
+    // track performance
+  }
+}
