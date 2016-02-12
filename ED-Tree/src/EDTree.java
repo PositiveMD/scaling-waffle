b9c8d3ef7f22e9f15ccf54501074821a0943434e
mahrab <barham@knights.ucf.edu>
2016-02-12 18:44:01 -0500

    Added a constructor and a tree checking method
    The tree checking method cheks if the balancers in the ED Tree exist.

--- ED-Tree/src/EDTree.java
+++ ED-Tree/src/EDTree.java
@@ -1,5 +1,40 @@
 /**
  * Created by Anthony Dinh on 2/11/2016.
  */
+ // Edited by Clayton Barham on 2/12/2016
 public class EDTree {
+  
+  // The root of the ED Tree
+	Balancer root;
+	
+	// A constructor
+	public EDTree(int depth)
+	{
+		root = new Balancer(depth);
+	}
+	
+	public void Check_Tree()
+	{
+		Balancer check = root;
+		for(int i = 1; i<=root.get_Depth();++i)
+		{
+			if(check!=null)
+			{
+				System.out.println(i);
+				check = check.Get_Left();
+			}
+			else
+			{
+				System.out.println("Broke Early: ");
+				System.out.println(i);
+				break;
+			}
+		}
+		System.out.println(root.get_Depth());
+	}
+	
+	public int get_depth()
+	{
+		return root.get_Depth();
+	}
 }
