575828f17b6790f398ef6c6ef6a62ea2dd9a7f8e
mahrab <barham@knights.ucf.edu>
2016-02-12 15:51:26 -0500

    Recursive child creation to Balancer Constructor

--- ED-Tree/src/Balancer.java
+++ ED-Tree/src/Balancer.java
@@ -1,6 +1,7 @@
 /**
  * Created by Anthony on 2/2/2016.
  */
+// Edited by Clayton Barham on 2/12/16 Note: My edtis have produced no build errors in elicpse, but have not been properly debugged yet.
 public class Balancer {
     public static final int ELIMINATIONARRAYSIZE = 10;
 
@@ -8,6 +9,9 @@ public class Balancer {
     Exchanger[] eliminationArray;
     Balancer leftChild, rightChild;
     ThreadLocal<Integer> lastSlotRange;
+    // the left and right children of thsi balancer, which will either be balancers or Concurrent Queues
+    Balancer left,right;
+    SynchronousQueue Qleft, Qright;
 
     //TODO : Figure out what order to do the linking for the left and right child
     public Balancer(){
@@ -15,5 +19,21 @@ public class Balancer {
         consumerToggle = new ToggleBit();
         eliminationArray = new Exchanger[ELIMINATIONARRAYSIZE];
         lastSlotRange = new ThreadLocal<>();
+        
+        // Recursively create children, if this is not the last layer of the ED-Tree, other wise add the queues
+        if(depth>1)
+        {
+        	left = new Balancer(--depth);
+        	right = new Balancer(--depth);
+        	Qleft = NULL;
+        	Qright = NULL;
+        }
+        else
+        {
+        	left = NULL;
+        	right = NULL;
+        	Qleft = new SynchronousQueue();
+        	Qright = new SynchronousQueue();
+        }
     }
 }
