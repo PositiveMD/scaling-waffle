9a7219283d921d98575df2762759dd9c4e822dd5
mahrab <barham@knights.ucf.edu>
2016-02-15 01:31:10 -0500

    added attempt at pushing function
    I'm getting a null pointer exception at line 75, when I try to put an exchanger package into the elimination array.

--- ED-Tree/src/Balancer.java
+++ ED-Tree/src/Balancer.java
@@ -1,4 +1,5 @@
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by Anthony on 2/2/2016.
@@ -11,8 +12,9 @@ public class Balancer {
     Exchanger[] eliminationArray;
     ThreadLocal<Integer> lastSlotRange;
     Balancer leftChild, rightChild;
-    ConcurrentLinkedQueue Qleft, Qright;
+    ConcurrentLinkedQueue<Integer> Qleft, Qright;
     int Bdepth;
+    int slot = 0;
 
     //TODO : Figure out what order to do the linking for the leftChild and rightChild child
     public Balancer(int depth){
@@ -34,8 +36,8 @@ public class Balancer {
         {
         	leftChild = null;
         	rightChild = null;
-        	Qleft = new ConcurrentLinkedQueue();
-        	Qright = new ConcurrentLinkedQueue();
+        	Qleft = new ConcurrentLinkedQueue<Integer>();
+        	Qright = new ConcurrentLinkedQueue<Integer>();
         }
     }
     
@@ -52,5 +54,109 @@ public class Balancer {
     public int get_Depth()
     {
     	return Bdepth;
+    }
+    
+    public boolean push(Integer n)
+    {
+    	// create the exchanger package to be pushed
+    	ExchangerPackage payload = new ExchangerPackage(n,State.WAITING,Type.PUSH);
+    	
+    	// attempt to access slots in the elimination array, spinning on a timer and 
+    	// trying successively smaller ranges of the array until the range hits size 
+    	// zero, at which point it toggles the bit and moves to a child balancer
+    	for(lastSlotRange.set(ELIMINATIONARRAYSIZE);lastSlotRange.get()>0;lastSlotRange.set(lastSlotRange.get()/2))
+    	{
+    		// pick a random slot in the slot range
+    		slot = ThreadLocalRandom.current().nextInt(lastSlotRange.get());
+    		
+    		if(eliminationArray[slot]==null)
+    		{
+    			// if the chosen slot is currently empty, publish the payload there
+    			eliminationArray[slot].slot.set(payload);
+    			
+    			// spin and wait for a collision
+    			for(int timer = 0; timer<100;++timer)
+    			{
+    				// check for collisions while we wait
+    				if(eliminationArray[slot]==null)
+    				{
+    					// if the slot is set back to null, then we collided with a pop
+        				// and we are done here.
+    					return true;
+    				}
+    				else if(eliminationArray[slot].slot.get().state == State.DIFFRACTED1)
+    				{
+    					// if the state is changed to diffracted1, then we collided with
+    					// another push, and we should move to the right child
+    					payload = eliminationArray[slot].slot.get();
+    					eliminationArray[slot].slot.set(null);
+    					if(rightChild!=null)
+    					{
+    						return rightChild.push((Integer) payload.value);
+    					}
+    					else
+    					{
+    						Qright.add((Integer) payload.value);
+    						return true;
+    					}
+    				}
+    			}
+    		}
+    		else if(eliminationArray[slot].slot.get().type==Type.POP)
+    		{
+    			// if we collide with a pop, we replace it with our push package and let
+    			// pop pick it up
+    			eliminationArray[slot].slot.set(payload);
+    			return true;
+    		}
+    		else if(eliminationArray[slot].slot.get().type==Type.PUSH)
+    		{
+    			// if we encounter another push operation at the slot, we diffract the 
+    			// two packages to opposite children, and leave the toggle alone
+    			eliminationArray[slot].slot.get().state = State.DIFFRACTED1;
+    			if(leftChild!=null)
+    			{
+    				return leftChild.push((Integer) payload.value);
+    			}
+    			else
+    			{
+    				Qleft.add((Integer) payload.value);
+    				return true;
+    			}
+    		}
+    	}
+    	
+    	// If the package exhausts the entire elimination range, toggle the bit and 
+    	// try one of the child balancers
+    	if(producerToggle.toogle())
+    	{
+    		// send to right child
+    		Integer m = (Integer) eliminationArray[slot].slot.get().value;
+    		eliminationArray[slot].slot.set(null);
+    		if(rightChild!=null)
+    		{
+    			return rightChild.push(m);
+    		}
+    		else
+    		{
+    			Qright.add((Integer) payload.value);
+				return true;
+    		}
+    	}
+    	else
+    	{
+    		// send to left child
+    		Integer m = (Integer) eliminationArray[slot].slot.get().value;
+    		eliminationArray[slot].slot.set(null);
+    		if(leftChild!=null)
+    		{
+    			return leftChild.push(m);
+    		}
+    		else
+    		{
+    			Qleft.add((Integer) payload.value);
+				return true;
+    		}
+    	}
     }
 }
